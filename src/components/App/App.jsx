8648a1a8882c300f6f28e6fd1c049003ff15450e
DanielaBadea <badeadaniella@gmail.com>
2024-05-22 21:55:40 +0300

    text default

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,16 +6,28 @@ import LayoutTasks from "components/LayoutTasksList/LayoutTasks";
 import StatusFilter from "components/StatusFilter/StatusFilster";
 import DeleteTasks from "components/DeleteTasks/DeleteTasks";
 import Counter from "components/CounterTasks/Counter";
+import { useSelector } from "react-redux";
+import { getTasks } from "../../redux/selectors";
+import css from './App.module.css'
 export const App = () => {
+
+  const tasks = useSelector(getTasks)
   return (
     <>
     <Form/>
-    <LayoutTasks>
-      <Counter/>
-    <StatusFilter/>
-    <TasksList/>
-    <DeleteTasks/>
-    </LayoutTasks>
+    {
+      tasks.length>0 ? (
+        <LayoutTasks>
+        <Counter/>
+      <StatusFilter/>
+      <TasksList/>
+      <DeleteTasks/>
+      </LayoutTasks>
+      ) : (
+        <p className={css.textDefault}>The list of tasks is empty!</p>
+      )
+    }
+   
     </>
   );
 };
